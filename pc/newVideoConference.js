8ac5bb04d47a423456949a48d9959a38f1596576
Ahrin Jun <wjsdkfls03@naver.com>
2024-11-13 18:00:11 +0900

    [arin] edit: 일렉트론 화상회의 창 크기 조절

--- pc/newVideoConference.js
+++ pc/newVideoConference.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, ipcRenderer} = require('electron');
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
 const path = require('path');
 
 let inviteUrl;
@@ -7,18 +7,18 @@ const createConferenceWindow = () => {
     const roomId = generateRoomId(); // 방 ID 생성
     const url = `https://vervet-sacred-needlessly.ngrok-free.app/roomId?roomId=${roomId}`; // URL에 방 ID 추가
     const win = new BrowserWindow({
-        width: 570,
-        height: 595,
+        width: 650,
+        height: 700,
         frame: false,  // 본 타이틀바 제거
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
-            enableRemoteModule: false, 
-            nodeIntegration: false 
+            enableRemoteModule: false,
+            nodeIntegration: false
         }
     });
     win.loadURL(url); // 서버에서 제공하는 URL을 로드
-    inviteUrl=url;
+    inviteUrl = url;
 };
 
 // 메모 추가 생성 버튼 누르면 창 추가 생성
@@ -29,7 +29,7 @@ ipcMain.handle('add-conference-window', () => {
 
 // 화상회의 초대 링크
 ipcMain.handle('invite-url', () => {
-    console.log('초대를 보낸 url: '+inviteUrl);
+    console.log('초대를 보낸 url: ' + inviteUrl);
     return inviteUrl;
 });
 
